fd24935f43157edcff4285be6d008bed50580506
nwmclaren <nwmclaren@aol.com>
2019-07-16 18:52:51 +0100

    MVP_Complete

---
+++ src/main/java/Library.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Library {
+    private int capacity;
+    private ArrayList<Book> books;
+
+    public Library(int capacity) {
+        this.capacity = capacity;
+        books = new ArrayList<Book>();
+    }
+
+    public int bookCount(){
+        return this.books.size();
+    }
+
+    public void addBook(Book book){
+        if (this.books.size() < this.capacity) {
+            this.books.add(book);
+        }
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class LibraryTest {
+
+    Library library;
+    Book book1;
+    Book book2;
+    Book book3;
+
+    @Before
+    public void setup(){
+    library = new Library(2);
+    book1 = new Book("2001", "Arthur C Clarke","Sci-Fi");
+    book2 = new Book("Gone Girl","Gilliam Flynn", "Thriller");
+    book3 = new Book("Romeo & Juliet","William Shakespeare", "Romance");
+    }
+
+    @Test
+    public void canCountBooks(){
+        assertEquals(0, library.bookCount());
+    }
+
+    @Test
+    public void canAddBook(){
+        assertEquals(0, library.bookCount());
+        library.addBook(book1);
+        assertEquals(1, library.bookCount());
+    }
+
+    @Test
+    public void canNotAddBookWhenFull(){
+        assertEquals(0, library.bookCount());
+        library.addBook(book1);
+        library.addBook(book2);
+        assertEquals(2,library.bookCount());
+        library.addBook(book3);
+        assertEquals(2,library.bookCount());
+    }
+
+
+}
